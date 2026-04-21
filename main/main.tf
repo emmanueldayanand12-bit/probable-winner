f555eee1c5493ed0a260a84682926f0460514835
emmanueldayanand12-bit <emmanuel.dayanand12@gmail.com>
2026-04-21 14:43:05 +0530

    Update main.tf

--- main/main.tf
+++ main/main.tf
@@ -2,3 +2,10 @@
 terraform {
   # No providers needed for this test
 }
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "jenkins-test-bucket-${random_id.id.hex}"
+}
+
+resource "random_id" "id" {
+  byte_length = 4
+}
